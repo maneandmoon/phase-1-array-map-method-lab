d096c33da12e18711af1d033940c0f978d46bfc2
LindaTa <lindata@maneandmoon.com>
2024-06-27 08:43:14 -0500

    array method lab w/rachel

--- index.js
+++ index.js
@@ -11,6 +11,74 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//const mySentence = tutorials [0];
+
+//const capFirstLetter = mySentence.split(" ").map((word) => {
+
+//  return (word [0].toUpperCase() + word.slice(1))
+
+//}).join(" ");
+
+
+//function titleCased () {
+
+ // return array.map(mySentence => mySentence.split(" ").map((word))); {
+
+ // return (word [0].toUpperCase() + word.slice(1));
+
+//  return mySentence.join (" ");
+
+//  const titleCased = () => {
+
+
+  //  return tutorials
+  //}
+//pseudo code
+//tutorials is global scope
+//split sentences in tutorials
+//capitalize each word
+//join sentences
+
+//function titleCased(){
+  //const capTutorials = tutorials.map(t => {
+    //const words = t.split(" ").map(w => {
+      //return w.charAt(0).toUpperCase() + w.slice(1);
+    //}).join(" ");
+    //console.log(words);
+    //return words; 
+  //})
+  //return capTutorials;
+  //return tutorials
+
+
 const titleCased = () => {
-  return tutorials
-}
+
+  return tutorials.map((curTutorial) => {
+
+    const wordArray = curTutorial.split (' ')
+
+    let capitalizedArray = wordArray.map((curWord) => {
+
+      return curWord = curWord[0].toUpperCase() + curWord.slice(1)
+
+      })
+      let capitalizedTutorial = capitalizedArray.join(' ')
+
+      return capitalizedTutorial
+   })
+
+ }
+
+ 
+
+  
+ 
+
+
+  
+
+    
+  
+ 
+
+
